1a7070e1af70ed8cc037df90fc856c7881564f57
caoliang1918 <caoliang1918@gmail.com>
2019-01-10 15:50:27 +0800

    添加测试文件

--- poi-example/src/main/java/com/zhongweixian/entity/ContactsEntity.java
+++ poi-example/src/main/java/com/zhongweixian/entity/ContactsEntity.java
@@ -16,25 +16,37 @@ public class ContactsEntity implements Serializable {
      * id
      */
     private java.lang.String id;
-    // 电话号码(主键)
+    /**
+     * 电话号码(主键)
+     */
     @Excel(name = "电话号码", width = 15)
     private String clientPhone = null;
-    // 客户姓名
+    /**
+     * 客户姓名
+     */
     @Excel(name = "姓名")
     private String clientName = null;
-    // 所属分组
+    /**
+     * 所属分组
+     */
     @ExcelEntity
     private ContactsGroup group = null;
-    // 备注
+    /**
+     * 备注
+     */
     @Excel(name = "备注")
     private String remark = null;
-    // 生日
+    /**
+     * 生日
+     */
     @Excel(name = "出生日期", format = "yyyy-MM-dd", width = 20)
     private Date birthday = null;
 
     @Excel(name = "创建时间", format = "yyyy-MM-dd HH:mm:ss", width = 25)
     private Date cts;
-    // 创建人
+    /**
+     * 创建人
+     */
     private String createBy = null;
 
     public String getId() {
